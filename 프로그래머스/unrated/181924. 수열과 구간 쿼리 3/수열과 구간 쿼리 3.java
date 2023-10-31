7396b9ec89fc9afca9bf3bf07cdfac31d61bfa72
DaHyun Ko <eorn21301@gmail.com>
2023-10-31 14:43:15 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int i=0; i<queries.length; i++) {
+            int index1 = queries[i][0];
+            int index2 = queries[i][1];
+            int temp = 0;
+            
+            temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+        return arr;
+    }
+}
