782e80e0a19527b87b9d1d9c8a828dec8462fd1a
DaHyun Ko <eorn21301@gmail.com>
2023-12-21 14:51:56 +0900

    [level unrated] Title: 로그인 성공?, Time: 0.03 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "fail";
+        for(String[] info : db) {
+            if(id_pw[0].equals(info[0])) {
+                answer = "wrong pw";
+                if(id_pw[1].equals(info[1])) {
+                    answer = "login";
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
